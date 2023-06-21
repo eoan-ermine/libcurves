499691b98b0c5087de36c8665feae82006f69702
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-21 21:32:57 +0300

    Remove fmt from dependencies, use boost as a dependency

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,6 @@ class Recipe(ConanFile):
         self.folders.generators = "conan"
 
     def requirements(self):
-        self.requires("fmt/9.1.0")
         self.requires("boost/1.82.0")
 
     def build_requirements(self):
