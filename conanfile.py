5e99c00a1a28d9f2d34f0b916781060a79688676
PatriotRossii <patriotrossii2019@mail.ru>
2023-06-21 21:09:34 +0300

    Add boost as a dependency

--- conanfile.py
+++ conanfile.py
@@ -10,6 +10,7 @@ class Recipe(ConanFile):
 
     def requirements(self):
         self.requires("fmt/9.1.0")
+        self.requires("boost/1.82.0")
 
     def build_requirements(self):
         self.test_requires("catch2/3.3.1")
